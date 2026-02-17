c7625862a4d775d51c91474a9c033e8466908429
yunipon <104571759+yunipon@users.noreply.github.com>
2026-02-18 01:00:30 +0900

    アナリティクスタグをheaderに追加

--- header.php
+++ header.php
@@ -17,3 +17,16 @@
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
 
 <link rel="icon" href="data:,">
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-3K3Z1HSGLF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+
+  gtag('config', 'G-3K3Z1HSGLF');
+</script>
