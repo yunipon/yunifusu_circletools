d3137767a87929b6a1ecd0f93613bda274242844
yunipon <104571759+yunipon@users.noreply.github.com>
2026-03-15 17:56:21 +0900

    headertools のコードを修正

--- page-tools/headertools.php
+++ page-tools/headertools.php
@@ -6,7 +6,7 @@
 <link rel="stylesheet" href="../styleMenu.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
-<script src="../common.js"></script>
+<script src="../script-tool/common.js"></script>
 
 <script src="https://unpkg.com/docx@7.1.0/build/index.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js"></script>
