6da212c524fb9a8484df98d6a7087ed496e3e465
yunipon <104571759+yunipon@users.noreply.github.com>
2026-03-22 16:40:52 +0900

    Xのフォローリンクを修正

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
         Ci-enで更新情報をチェック
       </a>
       <div class="x-timeline-container">
-        <a href="https://twitter.com/x?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-size="large" data-show-screen-name="false" data-show-count="false">Follow @x</a>
+        <a href="https://twitter.com/nyu_nyu_fantasy?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-size="large" data-show-screen-name="false" data-show-count="false">Follow @x</a>
         <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
       </div>
 
